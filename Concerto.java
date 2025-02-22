86c8ef9c9bb8a982e473dc9d88a2ce66771d6483
simonacorreale <simona.correale.work@gmail.com>
2025-02-22 08:31:59 +0100

    feat: creazioni delle classi in step 1 2 e 3

---
+++ Concerto.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Concerto extends Evento {
+
+    public Concerto(String titolo, LocalDate data, int postiTotali) {
+        super(titolo, data, postiTotali);
+        
+    }
+    
+}
